6ba737eb958f7d2e537326e6a303b22e0bda6421
jsegrestin <jules.segrestin@cefe.cnrs.fr>
2016-10-05 14:58:14 +0200

    rename within function to btw and add sbtw function

---
+++ R/btw.R
@@ -0,0 +1,29 @@
+#' Operator to see if value between values of vector
+#'
+#' Custom operator that takes a value and a numeric vector, returns \code{TRUE}
+#' if values between minimum and maximum of vector.
+#'
+#' @usage  x %btw% vec
+#' @usage x %sbtw% vec
+#'
+#' @param x a numeric value or vector
+#' @param vec a numeric vector
+#'
+#' @examples
+#'
+#' my_vec <- c(1, 4, 8)
+#'
+#' 1 %btw% my_vec  # returns TRUE
+#' 1.3 %btw% my_vec  # returns TRUE
+#' 0 %btw% my_vec  # returns FALSE
+#' c(0,1.3,8, 12) %sbtw% my_vec # returns FALSE TRUE FALSE FALSE
+#'
+#' @export
+
+"%btw%" <- function(x, vec) {
+  (x >= min(vec, na.rm=TRUE)) & (x <= max(vec, na.rm=TRUE))
+}
+
+"%sbtw%"<-function (x, vec) {
+  (x > min(vec, na.rm=TRUE)) & (x < max(vec, na.rm=TRUE))
+}
