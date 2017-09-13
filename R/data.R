50c110395b19e86b3fa694b7e0062d8c89245056
Rekyt <matthias.grenie@ens-lyon.fr>
2017-09-13 14:34:24 +0200

    Add documentation for data

---
+++ R/data.R
@@ -0,0 +1,30 @@
+#' LES dataset
+#'
+#' A dataset containing measures of LES.
+#'
+#' @format A data frame with 2548 rows and 21 variables:
+#' \describe{
+#'   \item{Code}{description}
+#'   \item{Dataset}{description}
+#'   \item{BIOME}{description}
+#'   \item{Species}{description}
+#'   \item{GF}{description}
+#'   \item{Decid_Egreen}{description}
+#'   \item{Needle_Broadlf}{description}
+#'   \item{C3C4}{description}
+#'   \item{N2_fixer}{description}
+#'   \item{log_LL}{description}
+#'   \item{log_LMA}{description}
+#'   \item{log_Nmass}{description}
+#'   \item{log_Narea}{description}
+#'   \item{log_Pmass}{description}
+#'   \item{log_Parea}{description}
+#'   \item{log_Amass}{description}
+#'   \item{log_Aarea}{description}
+#'   \item{log_Gs}{description}
+#'   \item{log_Rdmass}{description}
+#'   \item{log_Rdarea}{description}
+#'   \item{Ca_Ci}{description}
+#'
+#' }
+"LES"
