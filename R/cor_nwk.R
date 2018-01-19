da222f9802d3a38d0ad4d9effeb2c95290110aa8
jsegrestin <jules.segrestin@cefe.cnrs.fr>
2018-01-19 14:03:29 +0100

    p.adjust in cor_nwk

--- R/cor_nwk.R
+++ R/cor_nwk.R
@@ -4,30 +4,37 @@
 #'
 #' @param dat a matrix or data.frame with variables in column
 #' @param labels custom label vector in the same order than column names
-#' @param order logical to order according to a clustering method
+#' @param order logical to order variables according to a clustering method
 #' @param method method used for the correlation test
-#' @param col 2 length vector of color names for positive and negative correlation
+#' @param pval.adjust method to adjust p-values in "holm", "hochberg", "hommel", "bonferroni", "BH", "BY", "fdr"
+#' @param col 2 length vector of colour names for positive and negative correlation
 #'
 #' @examples
-#'
 #' data("mtcars")
 #' cor_nwk(mtcars)
 #'
 #' @import graphics
 #' @importFrom corrplot corrMatOrder
 #' @importFrom utils combn
+#' @importFrom stats p.adjust
 #'
 #' @author Jules Segrestin
 #'
 #' @export
 
 
-cor_nwk <- function(dat, labels = NA, method = "pearson", order = TRUE, col = c(1, "grey80")){
+cor_nwk <- function(dat, labels = NA, method = "pearson", pval.adjust = NULL,
+                    order = TRUE, col = c(1, "grey80")){
 
   if(!is.na(labels)) colnames(dat) <- labels
   value <- cor(dat, method = method)
   if (order) dat <- dat[, corrMatOrder(value)]
   value <- cor(dat, method = method)
+  if (! is.null(pval.adjust) &
+      ! pval.adjust %in% c("holm", "hochberg", "hommel",
+                           "bonferroni", "BH", "BY", "fdr", "none")){
+    stop("pval.adjust incorrect")
+  }
 
   angles <- seq(0, 2*pi, length.out = ncol(dat)+1)[-1]
   pts <- data.frame(var = colnames(dat), x = sin(angles), y = cos(angles))
@@ -44,18 +51,23 @@ cor_nwk <- function(dat, labels = NA, method = "pearson", order = TRUE, col = c(
   }
   text(pts$x * 1.1, pts$y * 1.1, colnames(dat), pos = pos)
 
-  df <- nrow(dat) - 2
-  critical.t <- qt( 0.05/2, df, lower.tail = F )
-  critical.r <- sqrt( (critical.t^2) / ( (critical.t^2) + df ) )
-
   allcomb <- combn(colnames(dat), 2)
   ordre <- order(abs(apply(allcomb, MARGIN = 2, function(x) value[x[1], x[2]])))
   allcomb <- allcomb[, ordre]
+
+  df <- nrow(dat) - 2
+  if (! is.null(pval.adjust)) p.seuil <- p.adjust(0.05, pval.adjust, n = ncol(allcomb))
+  critical.t <- qt(p.seuil/2, df, lower.tail = F)
+  critical.r <- sqrt((critical.t^2) / ( (critical.t^2) + df))
+
   wd <- seq(critical.r, 1, length.out = 6)
+
   for (i in 1:ncol(allcomb)){
     test <- cor.test(dat[, allcomb[1, i]], dat[, allcomb[2, i]], method = method)
     val <- test$estimate
     pval <- test$p.value
+    if (! is.null(pval.adjust)) pval <- p.adjust(pval, pval.adjust, n = ncol(allcomb))
+
     lwd <- which(wd < abs(val))
     if(length(lwd) == 0 & pval < 0.05) lwd <- 1
     coli <- ifelse(val > 0, col[1], col[2])
@@ -63,5 +75,6 @@ cor_nwk <- function(dat, labels = NA, method = "pearson", order = TRUE, col = c(
     if(length(lwd) > 0) segments(loc[1,1], loc[1,2], loc[2,1], loc[2,2], lwd = max(lwd), col = coli)
     if(pval < 0.1 & pval > 0.05) segments(loc[1,1], loc[1,2], loc[2,1], loc[2,2], lty = 3, col = coli)
   }
+
   points(pts$x, pts$y, pch = 16)
 }
